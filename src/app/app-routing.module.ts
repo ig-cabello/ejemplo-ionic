7b2f6aaa283527a19544cbf2220b0a5d83369b39
ig-cabello <ig.cabello@duocuc.cl>
2021-08-25 03:32:00 -0400

    confirmo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,18 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'pagina1',
+    loadChildren: () => import('./pages/pagina1/pagina1.module').then( m => m.Pagina1PageModule)
+  },
+  {
+    path: 'pagina2',
+    loadChildren: () => import('./pages/pagina2/pagina2.module').then( m => m.Pagina2PageModule)
+  },
+  {
+    path: 'pagina3',
+    loadChildren: () => import('./pages/pagina3/pagina3.module').then( m => m.Pagina3PageModule)
+  },
 ];
 
 @NgModule({
